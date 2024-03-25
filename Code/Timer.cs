f20196bc72c49146fd821b8bcced1e991ecbba8d
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-03-25 17:49:41 -0400

    Created simple timer
    Made a simple countdown timer, you can change timeRemaining variable to the desired amount.
    
    Uses text mesh pro, hopefully not an issue. If it is I can change it.
    
    To use, drag the Timer.cs file onto the text and then drag the text into the "Time Text" box in the script section and it should work.

---
+++ Code/Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 120;
+    public bool timeIsRunning = true;
+    public TMP_Text timeText;
+
+    void Start()
+    {
+        timeIsRunning = true;
+        DisplayTime(timeRemaining);
+    }
+
+    void Update()
+    {
+        if (timeIsRunning){
+            if (timeRemaining > 0) {
+                DisplayTime(timeRemaining);
+                timeRemaining -= Time.deltaTime;
+            }
+        }
+    }
+    void DisplayTime (float timeToDisplay){
+        timeToDisplay -= 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
